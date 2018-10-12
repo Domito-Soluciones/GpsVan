d4ceac08b753d7869d62677584aea2dd0f630127
j0s3-s4nch3z <frogapp2019@gmail.com>
2018-10-12 00:30:48 -0300

    comit

---
+++ source/httprequest/LoginConductor.php
@@ -0,0 +1,17 @@
+<?php
+
+include '../query/AgenteDao.php';
+
+$respuesta = '0';
+$nombre = $_REQUEST['usuario'];
+$password = $_REQUEST['password'];
+$agenteDao = new AgenteDao();
+$agente = $agenteDao->getAgente($nombre, $password);
+if ($agente->getId() > 0)
+{
+    session_start();
+    $_SESSION['agente']=$agente->getId();
+    $respuesta = $_SESSION['agente'];
+}
+
+echo $respuesta;
