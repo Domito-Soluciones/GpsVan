103717f7825e86eecca0d0d63af5e535d58d2297
Jose Sanchez <jsanchez@adportas.cl>
2019-01-10 19:32:03 -0300

    se agrego post a pasjeros

--- source/httprequest/pasajero/DelPasajero.php
+++ source/httprequest/pasajero/DelPasajero.php
@@ -3,6 +3,7 @@ include '../../util/validarPeticion.php';
 include '../../util/validarSession.php';
 include '../../query/PasajeroDao.php';
 
-$rut = $_REQUEST['rut'];
+$rut = filter_input(INPUT_POST, 'rut');
 $pasajeroDao = new PasajeroDao();
 $pasajeroDao->eliminarPasajero($rut);
+echo "{\"pasajero_eliminado\":\"".$rut."\"}";
